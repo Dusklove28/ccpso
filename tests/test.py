4e15904c108289f340c80d0fb6483b6bd2fd2ee6
Dusklove28 <1774799513@qq.com>
2026-08-04 11:37:54 +0800

    首次提交

--- tests/test.py
+++ tests/test.py
@@ -1,33 +1,10 @@
 import numpy as np
 
-from learning_ddpg.swarm.ccpso import CCPSOSwarm
+from cec2017.functions import f5
 
-
-def sphere(positions):
-    return np.sum(positions ** 2, axis=1)
-
-
-swarm = CCPSOSwarm(
-    particles=20,
-    dimensions=10,
-    fun=sphere,
-    lower_bound=-100.0,
-    upper_bound=100.0,
-    max_fe=1000,
-)
-
-swarm.reset()
-
-gbest_history = [swarm.gbest_fitness]
-
-while not swarm.done:
-    swarm.step(conv=0.75)
-    gbest_history.append(swarm.gbest_fitness)
-
-print("FE:", swarm.fe_count)
-print("initial gbest:", gbest_history[0])
-print("final gbest:", gbest_history[-1])
-
-assert swarm.fe_count == 1000
-assert np.all(np.diff(gbest_history) <= 1e-12)
-assert swarm.gbest_fitness <= gbest_history[0]
+samples = 3
+dimension = 50
+x = np.random.uniform(-100, 100, size=(samples, dimension))
+val = f5(x)
+for i in range(samples):
+    print(f"f5(x_{i}) = {val[i]:.6f}")
